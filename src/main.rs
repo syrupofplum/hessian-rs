2eef44bcd03a26746754fe95bdb061f519c9af41
syrupofplum <syrupofplum@gmail.com>
2022-09-22 22:49:18 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use serde::{ser, Serialize};
 use serde::ser::{SerializeMap, SerializeSeq, SerializeStruct, SerializeStructVariant, SerializeTuple, SerializeTupleStruct, SerializeTupleVariant, StdError};
 use bytes::{BytesMut, BufMut};
 
+type BytesBuf = BytesMut;
+
 pub struct Error {
 
 }
@@ -158,115 +160,115 @@ where
     type SerializeStruct = SerializeResult;
     type SerializeStructVariant = SerializeResult;
 
-    fn serialize_bool(self, v: bool) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_bool(self, v: bool) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_i8(self, v: i8) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_i8(self, v: i8) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_i16(self, v: i16) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_i16(self, v: i16) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_i32(self, v: i32) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_i32(self, v: i32) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_i64(self, v: i64) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_i64(self, v: i64) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_u8(self, v: u8) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_u8(self, v: u8) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_u16(self, v: u16) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_u16(self, v: u16) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_u32(self, v: u32) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_u32(self, v: u32) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_u64(self, v: u64) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_u64(self, v: u64) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_f32(self, v: f32) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_f32(self, v: f32) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_f64(self, v: f64) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_f64(self, v: f64) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_char(self, v: char) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_char(self, v: char) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_str(self, v: &str) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_str(self, v: &str) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_bytes(self, v: &[u8]) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_none(self) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_none(self) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_some<T: ?Sized>(self, value: &T) -> std::result::Result<Self::Ok, Self::Error> where T: Serialize {
+    fn serialize_some<T: ?Sized>(self, value: &T) -> Result<Self::Ok> where T: Serialize {
         todo!()
     }
 
-    fn serialize_unit(self) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_unit(self) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_unit_struct(self, name: &'static str) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_unit_struct(self, name: &'static str) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_unit_variant(self, name: &'static str, variant_index: u32, variant: &'static str) -> std::result::Result<Self::Ok, Self::Error> {
+    fn serialize_unit_variant(self, name: &'static str, variant_index: u32, variant: &'static str) -> Result<Self::Ok> {
         todo!()
     }
 
-    fn serialize_newtype_struct<T: ?Sized>(self, name: &'static str, value: &T) -> std::result::Result<Self::Ok, Self::Error> where T: Serialize {
+    fn serialize_newtype_struct<T: ?Sized>(self, name: &'static str, value: &T) -> Result<Self::Ok> where T: Serialize {
         todo!()
     }
 
-    fn serialize_newtype_variant<T: ?Sized>(self, name: &'static str, variant_index: u32, variant: &'static str, value: &T) -> std::result::Result<Self::Ok, Self::Error> where T: Serialize {
+    fn serialize_newtype_variant<T: ?Sized>(self, name: &'static str, variant_index: u32, variant: &'static str, value: &T) -> Result<Self::Ok> where T: Serialize {
         todo!()
     }
 
-    fn serialize_seq(self, len: Option<usize>) -> std::result::Result<Self::SerializeSeq, Self::Error> {
+    fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq> {
         todo!()
     }
 
-    fn serialize_tuple(self, len: usize) -> std::result::Result<Self::SerializeTuple, Self::Error> {
+    fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple> {
         todo!()
     }
 
-    fn serialize_tuple_struct(self, name: &'static str, len: usize) -> std::result::Result<Self::SerializeTupleStruct, Self::Error> {
+    fn serialize_tuple_struct(self, name: &'static str, len: usize) -> Result<Self::SerializeTupleStruct> {
         todo!()
     }
 
-    fn serialize_tuple_variant(self, name: &'static str, variant_index: u32, variant: &'static str, len: usize) -> std::result::Result<Self::SerializeTupleVariant, Self::Error> {
+    fn serialize_tuple_variant(self, name: &'static str, variant_index: u32, variant: &'static str, len: usize) -> Result<Self::SerializeTupleVariant> {
         todo!()
     }
 
-    fn serialize_map(self, len: Option<usize>) -> std::result::Result<Self::SerializeMap, Self::Error> {
+    fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap> {
         todo!()
     }
 
-    fn serialize_struct(self, name: &'static str, len: usize) -> std::result::Result<Self::SerializeStruct, Self::Error> {
+    fn serialize_struct(self, name: &'static str, len: usize) -> Result<Self::SerializeStruct> {
         todo!()
     }
 
-    fn serialize_struct_variant(self, name: &'static str, variant_index: u32, variant: &'static str, len: usize) -> std::result::Result<Self::SerializeStructVariant, Self::Error> {
+    fn serialize_struct_variant(self, name: &'static str, variant_index: u32, variant: &'static str, len: usize) -> Result<Self::SerializeStructVariant> {
         todo!()
     }
 }
@@ -275,11 +277,33 @@ pub struct Formatter {
 }
 
 impl Formatter {
-    pub fn format_bool(v: bool, buf: &mut BytesMut) -> std::result::Result<(), Error> {
+    pub fn format_bool(v: bool, buf: &mut BytesBuf) -> Result<()> {
         match v {
-            true => {},
-            false => {},
+            true => buf.put_u8(0x54),
+            false => buf.put_u8(0x46),
         }
+        Ok(())
+    }
+
+    pub fn format_i8(v: i8, buf: &mut BytesBuf) -> Result<()> {
+        Formatter::format_i32(v.into(), buf)
+    }
+
+    pub fn format_i32(v: i32, buf: &mut BytesBuf) -> Result<()> {
+        match v {
+            -16..=47 => {
+                buf.put_i8((v + 0x90) as i8);
+            },
+            -2048..=2047 => {
+
+            },
+            -262144..=262143 => {
+
+            },
+            _ => {
+
+            },
+        };
         Ok(())
     }
 }
