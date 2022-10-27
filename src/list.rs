3f726fd65aea59bcf8fb5e784575738d6919566b
syrupofplum <syrupofplum@gmail.com>
2022-10-28 00:42:21 +0800

    update

--- src/list.rs
+++ src/list.rs
@@ -1,11 +1,34 @@
-use crate::constants::PrimitivesType::Int;
-use phf::phf_map;
+use serde::{Serialize, Serializer};
 use crate::constants::PrimitivesType;
+use crate::value::Value;
 
 pub enum List {
-    TypedList,
-    UntypedList,
+    TypedList(String, Vec<Value>),
+    UntypedList(Vec<Value>),
 }
 
-// static PRIMITIVES_LIST_TYPE: phf::Map<u8, &'static str> = phf_map! {
-// };
+fn get_primitive_type_str(primitive_type: PrimitivesType) -> &'static str {
+    return match primitive_type {
+        PrimitivesType::Byte => "[byte",
+        PrimitivesType::Short => "[short",
+        PrimitivesType::Int => "[int",
+        PrimitivesType::Long => "[long",
+        PrimitivesType::Float => "[float",
+        PrimitivesType::Double => "[double",
+        PrimitivesType::Boolean => "[boolean",
+        PrimitivesType::Char => "[char",
+    }
+}
+
+impl Serialize for List {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer
+    {
+        match self {
+            List::TypedList(m_type,value_list) => {},
+            List::UntypedList(value_list) => {},
+        }
+        Ok(())
+    }
+}
